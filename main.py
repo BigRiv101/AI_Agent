539ac8ebcb341f401e04a625d2be409bc0c7900e
BigRiv101 <toledord@grace.edu>
2026-01-28 17:53:55 -0500

    Added Class

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ import lmstudio as lms
 load_dotenv()
 
 
-api_host = lms.Client.find_default_local_api_host() or "http://127.0.0.1:4000"
-lms.configure_default_client(api_host)
+class Agent:
 
-model = lms.llm()
+    def __init__(self):
+        self.api_host = lms.Client.find_default_local_api_host() or "http://127.0.0.1:4000"
+        lms.configure_default_client(self.api_host)
+        self.model = lms.llm()
+        self.message = lms.Chat("You are a helpful assistant.")
 
-chat = lms.Chat("You are a helpful assistant.")  
-chat.add_user_message("Hello how are you?")
-response = model.respond(chat)
-print(response)
+
+    def chat(self, message):
+        
+        self.message.add_user_message(message)
+        response = self.model.respond(self.message)
+        print(response)
+
+agent = Agent()   
+agent.chat("Hello, how are you?")
